55c490ea130517fdaeb6dfb453edf70eedd0085b
mrhatimdarbali <125483529+mrhatimdarbali@users.noreply.github.com>
2024-04-04 14:47:57 +0000

    o

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, '../src/index.js'), // Adjusted the path to the entry file
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'), // Adjusted the path to the output directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Removed unnecessary single quotes
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/, // Removed unnecessary single quotes
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(ico|gif|png|jpg|jpeg)$/i, // Corrected regular expression
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devServer: {
+    static: path.resolve(__dirname, '../dist'), // Adjusted the path to the static files directory
+    compress: true,
+    open: true,
+    hot: true,
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My App',
+    }),
+  ],
+};
