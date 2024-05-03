fc6ab39efee1aa7ad86cc74ac66b3dc29a468770
mrhatimdarbali <125483529+mrhatimdarbali@users.noreply.github.com>
2024-05-03 16:51:47 +0100

    o

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/courseReducer.test.js
@@ -0,0 +1,50 @@
+import courseReducer from './courseReducer';
+import { FETCH_COURSE_SUCCESS, SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
+
+describe('courseReducer', () => {
+  it('should return the initial state', () => {
+    expect(courseReducer(undefined, {})).toEqual([]);
+  });
+
+  it('should handle FETCH_COURSE_SUCCESS', () => {
+    const courses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 }
+    ];
+    const action = { type: FETCH_COURSE_SUCCESS, data: courses };
+    expect(courseReducer([], action)).toEqual([
+      { id: 1, name: 'ES6', credit: 60, isSelected: false },
+      { id: 2, name: 'Webpack', credit: 20, isSelected: false },
+      { id: 3, name: 'React', credit: 40, isSelected: false }
+    ]);
+  });
+
+  it('should handle SELECT_COURSE', () => {
+    const initialState = [
+      { id: 1, name: 'ES6', isSelected: false },
+      { id: 2, name: 'Webpack', isSelected: false },
+      { id: 3, name: 'React', isSelected: false }
+    ];
+    const action = { type: SELECT_COURSE, index: 2 };
+    expect(courseReducer(initialState, action)).toEqual([
+      { id: 1, name: 'ES6', isSelected: false },
+      { id: 2, name: 'Webpack', isSelected: true },
+      { id: 3, name: 'React', isSelected: false }
+    ]);
+  });
+
+  it('should handle UNSELECT_COURSE', () => {
+    const initialState = [
+      { id: 1, name: 'ES6', isSelected: false },
+      { id: 2, name: 'Webpack', isSelected: true },
+      { id: 3, name: 'React', isSelected: false }
+    ];
+    const action = { type: UNSELECT_COURSE, index: 2 };
+    expect(courseReducer(initialState, action)).toEqual([
+      { id: 1, name: 'ES6', isSelected: false },
+      { id: 2, name: 'Webpack', isSelected: false },
+      { id: 3, name: 'React', isSelected: false }
+    ]);
+  });
+});
